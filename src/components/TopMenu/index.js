20f570e3a0d5eb6e8dc3242409f6690bb61b37cb
Candy Fair <candice.bastien@gmail.com>
2022-02-18 19:39:39 +0100

    Add links and elevation to TopMenu

--- src/components/TopMenu/index.js
+++ src/components/TopMenu/index.js
@@ -1,64 +1,82 @@
 // === IMPORTS
+import { useNavigate, Link } from 'react-router-dom';
+
 import { makeStyles } from '@mui/styles';
-import {
-  Stack, AppBar, IconButton,
-} from '@mui/material';
+import { Stack, AppBar, Box } from '@mui/material';
 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+
+import TopMenuOffset from 'src/components/TopMenu/TopMenuOffset';
+import ElevationScroll from 'src/utils/utils';
 
 import logo from '../../assets/images/logo.png';
 
 // === MUI
 const useStyles = makeStyles((theme) => ({
+  navColor: {
+    background: theme.palette.background.paper,
+  },
   navStyle: {
     padding: theme.spacing(1),
   },
-  iconStyle: {
+  github: {
     color: theme.palette.primary.main,
+    fontSize: 35,
   },
-
+  linkedin: {
+    color: theme.palette.primary.main,
+    fontSize: 38,
+  },
 }));
 
 // === COMPONENT
-const TopMenu = () => {
+const TopMenu = (props) => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   return (
-    <AppBar
-      position="static"
-      color="transparent"
-      elevation={0}
-    >
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        className={classes.navStyle}
-      >
-        <img
-          src={logo}
-          alt="Logo Candice's portfolio"
-        />
-
-        <Stack
-          direction="row"
+    <Box>
+      <ElevationScroll {...props}>
+        <AppBar
+          position="fixed"
+          elevation={0}
+          className={classes.navColor}
         >
-          <IconButton
-            className={classes.iconStyle}
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            className={classes.navStyle}
           >
-            <GitHubIcon />
-          </IconButton>
+            <img
+              src={logo}
+              alt="Go to Homepage"
+              onClick={() => navigate('/')}
+            />
 
-          <IconButton
-            className={classes.iconStyle}
-          >
-            <LinkedInIcon />
-          </IconButton>
-        </Stack>
+            <Stack
+              direction="row"
+              gap={1}
+            >
+              <a href="https://github.com/Candyfair" target="_blank" rel="noreferrer">
+                <GitHubIcon
+                  className={classes.github}
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/candicebfairand/" target="_blank" rel="noreferrer">
+                <LinkedInIcon
+                  className={classes.linkedin}
+                />
+              </a>
+            </Stack>
 
-      </Stack>
-    </AppBar>
+          </Stack>
+        </AppBar>
+      </ElevationScroll>
+
+      <TopMenuOffset />
+    </Box>
   );
 };
 
---
+++ src/utils/utils.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+export default function ElevationScroll(props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+};
